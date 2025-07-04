4c13e51ca60bc4582a119b6b150f0db14f536376
Gurpreet Singh <gurpreetsingh4174@gmail.com>
2025-07-04 19:12:18 +0530

    added a ques

---
+++ ques10.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num, sum=0;
+    cout<<"Enter a Number: "<<endl;
+    cin>>num;
+    while (num!=0)
+    {
+        int digit = num%10; //get last digit
+        sum+=digit; //add to sum
+        num/=10;// remove last digit 
+    }
+    cout<<"Sum of digit: "<<sum<<endl;
+
+}
