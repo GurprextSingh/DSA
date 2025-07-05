68b90c9afbd51e556696dbee2c41df1793a37b32
Gurpreet Singh <gurpreetsingh4174@gmail.com>
2025-07-05 15:46:20 +0530

    Added files

--- ques12.cpp
+++ ques12.cpp
@@ -1,18 +1,17 @@
-//WAP to reverse a number
-#include<iostream>
+// WAP to reverse a number
+#include <iostream>
 using namespace std;
-int main(){
-    int num,reverse=0;
-    cout<<"Enter a Number: ";
-    cin>>num;
-    
-    for (int i = 0; i <num; i++)
-    {
-       int digit=num%10; // Gives last digit
-       reverse=reverse*10+digit; // Add number;
-       num=num/10; //remove digits
-    
-    }
-    cout<<"Reverse of given number is "<<reverse;
+int main()
+{
+    int num, reverse = 0;
+    cout << "Enter a Number: ";
+    cin >> num;
 
+    while (num != 0)
+    {
+        int digit = num % 10;           // Gives last digit
+        reverse = reverse * 10 + digit; // Add number;
+        num = num / 10;                 // remove digits
+    }
+    cout << "Reverse of given number is " << reverse;
 }
