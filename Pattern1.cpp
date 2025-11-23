29fc0d9ad11990f64c79a07b5c18270a49bfe471
Gurpreet Singh <gurpreetsingh4174@gmail.com>
2025-11-23 22:48:40 +0530

    Started

---
+++ Pattern1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // WAP to Print the follwing Pattern
+    //  * * * *
+    //  * * * *
+    //  * * * *
+    //  * * * *
+    for (int  i = 0; i < 4; i++){
+        for ( int j = 0; j <4; j++)
+        {
+           cout<<"* ";
+        }
+        cout<<endl;
+    }
+     
+    }
+    
+
