7461a9fea9aba86bdfd05ad8620d9cf60f67c70f
Gurpreet Singh <gurpreetsingh4174@gmail.com>
2025-11-24 00:14:05 +0530

    Added patterns

---
+++ Pattern7.cpp
@@ -0,0 +1,20 @@
+/* WAP to Print the Follwoing Pattern
+1
+2 1
+3 2 1
+4 3 2 1 
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n=4;
+    for (int i = 1; i <=n; i++)
+    {
+      for (int j = i;  j>0; j--)
+      {
+      cout<<j<<" ";
+      }
+      cout<<endl;
+    }
+    
+}
