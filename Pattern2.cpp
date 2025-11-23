8a3b8cf5ff7449a07d0b93718a33ad681b7a4eb5
Gurpreet Singh <gurpreetsingh4174@gmail.com>
2025-11-23 22:56:05 +0530

    2nd QUes

---
+++ Pattern2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //WAP to Print the Follwing Pattern
+    // 1 2 3 4
+    // 1 2 3 4
+    // 1 2 3 4
+    // 1 2 3 4
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=4;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+
+}
