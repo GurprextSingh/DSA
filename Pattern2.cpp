f9a005f4e9c22b2488dd8f2a31980d2531cf0d31
Gurpreet Singh <gurpreetsingh4174@gmail.com>
2025-11-23 23:09:01 +0530

    added Char pattern

--- Pattern2.cpp
+++ Pattern2.cpp
@@ -12,5 +12,19 @@ int main(){
         }
         cout<<endl;
     }
+    cout<<endl;
+    // For Character Pattern
+    // A B C D
+    // A B C D
+    // A B C D
+    // A B C D
+    for(int i=1;i<=4;i++){
+        char ch=65;
+        for(int j=1;j<=4;j++){
+            cout<<ch<<" ";
+            ch+=1;
+        }
+        cout<<endl;
+    }
 
 }
