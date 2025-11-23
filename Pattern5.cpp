670e48ec34a63c1f3fbccf3b0810e1d65543124a
Gurpreet Singh <gurpreetsingh4174@gmail.com>
2025-11-23 23:47:26 +0530

    Added Character Pattern of Same

--- Pattern5.cpp
+++ Pattern5.cpp
@@ -4,6 +4,11 @@
 3 3 3
 4 4 4 4
  */
+ /* Also print Character pattern for 
+ A 
+ B B 
+ C C C 
+ D D D D */
 #include<iostream>
 using namespace std;
 int main(){
@@ -15,6 +20,17 @@ int main(){
         cout<<num<<" ";
        }
        num++;
+       cout<<endl;
+    }
+    cout<<endl;
+    char ch=65;
+    for (int i = 0; i <n; i++)
+    {
+       for (int j = 0; j <=i; j++)
+       {
+        cout<<ch<<" ";
+       }
+       ch++;
        cout<<endl;
     }
     
